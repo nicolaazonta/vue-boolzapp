887c7bfd79b78eb331920221d8076b2b8fd1cc99
nicolaazonta <nicolazonta114@gmail.com>
2023-04-03 20:40:40 +0200

    view correct time on messages

--- Milestone-1&2&3/assets/js/app.js
+++ Milestone-1&2&3/assets/js/app.js
@@ -12,17 +12,17 @@ createApp({
                     visible: true,
                     messages: [
                         {
-                            date: '10/01/2020 15:30:55',
+                            date: '15:30',
                             message: 'Hai portato a spasso il cane?',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 15:50:00',
+                            date: '15:50',
                             message: 'Ricordati di stendere i panni',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 16:15:22',
+                            date: '16:15',
                             message: 'Tutto fatto!',
                             status: 'received'
                         }
@@ -34,17 +34,17 @@ createApp({
                     visible: true,
                     messages: [
                         {
-                            date: '20/03/2020 16:30:00',
+                            date: '16:30',
                             message: 'Ciao come stai?',
                             status: 'sent'
                         },
                         {
-                            date: '20/03/2020 16:30:55',
+                            date: '16:30',
                             message: 'Bene grazie! Stasera ci vediamo?',
                             status: 'received'
                         },
                         {
-                            date: '20/03/2020 16:35:00',
+                            date: '16:35',
                             message: 'Mi piacerebbe ma devo andare a fare la spesa.',
                             status: 'sent'
                         }
@@ -56,17 +56,17 @@ createApp({
                     visible: true,
                     messages: [
                         {
-                            date: '28/03/2020 10:10:40',
+                            date: '10:10',
                             message: 'La Marianna va in campagna',
                             status: 'received'
                         },
                         {
-                            date: '28/03/2020 10:20:10',
+                            date: '10:20',
                             message: 'Sicuro di non aver sbagliato chat?',
                             status: 'sent'
                         },
                         {
-                            date: '28/03/2020 16:15:22',
+                            date: '16:15',
                             message: 'Ah scusa!',
                             status: 'received'
                         }
@@ -78,12 +78,12 @@ createApp({
                     visible: true,
                     messages: [
                         {
-                            date: '10/01/2020 15:30:55',
+                            date: '15:30',
                             message: 'Lo sai che ha aperto una nuova pizzeria?',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 15:50:00',
+                            date: '15:50',
                             message: 'Si, ma preferirei andare al cinema',
                             status: 'received'
                         }
@@ -95,12 +95,12 @@ createApp({
                     visible: true,
                     messages: [
                         {
-                            date: '10/01/2020 15:30:55',
+                            date: '15:30',
                             message: 'Ricordati di chiamare la nonna',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 15:50:00',
+                            date: '15:50',
                             message: 'Va bene, stasera la sento',
                             status: 'received'
                         }
@@ -112,17 +112,17 @@ createApp({
                     visible: true,
                     messages: [
                         {
-                            date: '10/01/2020 15:30:55',
+                            date: '15:30',
                             message: 'Ciao Claudia, hai novità?',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 15:50:00',
+                            date: '15:50',
                             message: 'Non ancora',
                             status: 'received'
                         },
                         {
-                            date: '10/01/2020 15:51:00',
+                            date: '15:51',
                             message: 'Nessuna nuova, buona nuova',
                             status: 'sent'
                         }
@@ -134,12 +134,12 @@ createApp({
                     visible: true,
                     messages: [
                         {
-                            date: '10/01/2020 15:30:55',
+                            date: '15:30',
                             message: 'Fai gli auguri a Martina che è il suo compleanno!',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 15:50:00',
+                            date: '15:50',
                             message: 'Grazie per avermelo ricordato, le scrivo subito!',
                             status: 'received'
                         }
@@ -151,17 +151,17 @@ createApp({
                     visible: true,
                     messages: [
                         {
-                            date: '10/01/2020 15:30:55',
+                            date: '15:30',
                             message: 'Ciao, andiamo a mangiare la pizza stasera?',
                             status: 'received'
                         },
                         {
-                            date: '10/01/2020 15:50:00',
+                            date: '15:50',
                             message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 15:51:00',
+                            date: '15:51',
                             message: 'OK!!',
                             status: 'received'
                         }
@@ -170,23 +170,25 @@ createApp({
             ]
         }        
     },
-    methods:{  
+    methods:{         
         selectContact(index){
             this.activeContact = index;
             //console.log('cliccato' + index);
         },
         sendMessage(){
-           let newMessage = {
-               date: '11:00',
-               message: this.textMessage,
-               status: 'sent' 
+            let time = new Date().toJSON().slice(11, 16);
+            console.log(time);
+
+            let newMessage = {
+                date: time,
+                message: this.textMessage,
+                status: 'sent' 
             };
             this.textMessage='';
             this.contacts[this.activeContact].messages.push(newMessage);
-            //setTimeout(randomMessage(), 1000)
             setTimeout(() => {
                     let newRandomMessage = {
-                    date: '11:00',
+                    date: time,
                     message: this.randomWords[Math.floor(Math.random()* this.randomWords.length)],
                     status: 'received' 
                  };
